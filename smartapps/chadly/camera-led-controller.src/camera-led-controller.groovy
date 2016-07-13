767b2edf6717f4377ec85fa43f81059365076db4
Chad Lee <chad.lee@archoninfosys.com>
2016-07-13 14:39:40 -0500

    Add date log to camera LED source

--- smartapps/chadly/camera-led-controller.src/camera-led-controller.groovy
+++ smartapps/chadly/camera-led-controller.src/camera-led-controller.groovy
@@ -12,6 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *	2016-07-10: Initial Release
  */
 definition(
 	name: "Camera LED Controller",
