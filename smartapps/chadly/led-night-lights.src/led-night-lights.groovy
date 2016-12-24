d69c605cea8873dde147d2855e0f2efb3eb92010
Chad Lee <git@chadly.net>
2016-12-23 20:55:46 -0600

    fixing log in comments

--- smartapps/chadly/led-night-lights.src/led-night-lights.groovy
+++ smartapps/chadly/led-night-lights.src/led-night-lights.groovy
@@ -12,8 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *	2016-11-23: Add option to force LED color
- *	2016-11-21: Initial Release
+ *	2016-12-23: Initial Release
  */
 definition(
 	name: "LED Night Lights",
